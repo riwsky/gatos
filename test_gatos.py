850780b744188ff6e5c9ca8af8b8160f3713db70
William Cybriwsky <william.cybriwsky@gmail.com>
2013-02-22 02:40:10 -0500

    Add another test

--- test_gatos.py
+++ test_gatos.py
@@ -37,6 +37,24 @@ class TestGato(unittest.TestCase):
         actual = starting_stack | rep
         self.assertEqual(expected, actual)
 
+    def test_partial(self):
+        starting_stack = [3,4,5,6]
+        expected =  [3,4,5,18]
+        actual = starting_stack | mult.partial(3)
+        self.assertEqual(expected, actual)
+
+        #needs to test kwarg stuff
+        starting_stack = [3,4,5,6]
+        
+        @Gato
+        def fake_fn(a,b,c):
+            print b,c
+            return a
+
+        expected = [3,4,6]
+        actual = starting_stack | fake_fn.partial(c=77)
+        self.assertEqual(expected, actual)
+
     def test_composition(self):
         starting_stack00 = [3,4,5,6]
         starting_stack01 = [3,4,5,6]
